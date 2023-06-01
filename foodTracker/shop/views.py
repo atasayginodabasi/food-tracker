9858597a831fa0970ae5c91901aa55a1a320b13c
atasayginodabasi <atasaygin13@gmail.com>
2023-06-01 22:19:49 +0300

    shop page

--- foodTracker/shop/views.py
+++ foodTracker/shop/views.py
@@ -5,6 +5,7 @@ sales = Blueprint('shop', __name__)
 
 stripe.api_key = "sk_test_4eC39HqLyjWDarjtT1zdp7dc"
 public_key = 'pk_test_TYooMQauvdEDq54NiTphI7jx'
+YOUR_DOMAIN = 'http://localhost:80'
 
 
 @sales.route('/shop', methods=['GET', 'POST'])
@@ -21,20 +22,34 @@ def thankyou():
 
 @sales.route('/payment', methods=['GET', 'POST'])
 def payment():
-    # CUSTOMER INFORMATION
-    customer = stripe.Customer.create(email=request.form['stripeEmail'],
-                                      source=request.form['stripeToken'])
+    try:
+        checkout_session = stripe.checkout.Session.create(
+            line_items=[
+                {
+                    # Provide the exact Price ID (for example, pr_1234) of the product you want to sell
+                    'price': "price_1NEGDD2eZvKYlo2Cpft6TINs",
+                    'quantity': 5,
+                },
+            ],
+            mode='payment',
+            success_url=YOUR_DOMAIN + '/thankyou',
+            cancel_url=YOUR_DOMAIN + '/shop',
+        )
+    except Exception as e:
+        return str(e)
 
-    # CHARGE/PAYMENT INFORMATION
-    charge = stripe.Charge.create(
-        customer=customer.id,
-        amount=2000,
-        currency='usd',
-        description='Shopping'
-    )
-
-    return redirect(url_for('shop.thankyou'))
+    return redirect(checkout_session.url, code=303)
 
 
 def basket():
     pass
+
+
+# stripe.Product.create(
+#   name="Strawberry",
+#   default_price_data={
+#     "unit_amount": 10000,
+#     "currency": "try",
+#   },
+#   expand=["default_price"],
+# )
