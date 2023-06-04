227be2f784b890a8b4da341ae7e34f3cfb8c7e67
atasayginodabasi <atasaygin13@gmail.com>
2023-06-04 23:23:59 +0300

    shop page

--- foodTracker/shop/views.py
+++ foodTracker/shop/views.py
@@ -1,5 +1,6 @@
-from flask import render_template, url_for, request, redirect, Blueprint
+from flask import render_template, session, url_for, request, redirect, Blueprint
 import stripe
+import pandas as pd
 
 sales = Blueprint('shop', __name__)
 
@@ -10,12 +11,20 @@ YOUR_DOMAIN = 'http://localhost:80'
 
 @sales.route('/shop', methods=['GET', 'POST'])
 def shop_page():
-    print(request.form.get('a'))
+    items = pd.DataFrame([i for i in stripe.Product.list(limit=50).data if i.default_price is not None])
+
+    prices = pd.DataFrame(stripe.Price.list(limit=50).data)
+
+    products = items[['id', 'default_price', 'name']] \
+        .merge(prices[['currency', 'product', 'unit_amount', 'unit_amount_decimal']], left_on='id', right_on='product') \
+        .drop_duplicates(subset=['name'], keep='first')
+
     return render_template("shop.html",
-                           public_key=public_key)
+                           public_key=public_key,
+                           products=products)
 
 
-@sales.route('/thankyou')
+@sales.route('/thank-you')
 def thankyou():
     return render_template('thankyou.html')
 
@@ -24,13 +33,7 @@ def thankyou():
 def payment():
     try:
         checkout_session = stripe.checkout.Session.create(
-            line_items=[
-                {
-                    # Provide the exact Price ID (for example, pr_1234) of the product you want to sell
-                    'price': "price_1NEGDD2eZvKYlo2Cpft6TINs",
-                    'quantity': 5,
-                },
-            ],
+            line_items=session['basket'],
             mode='payment',
             success_url=YOUR_DOMAIN + '/thankyou',
             cancel_url=YOUR_DOMAIN + '/shop',
@@ -41,15 +44,34 @@ def payment():
     return redirect(checkout_session.url, code=303)
 
 
+@sales.route('/basket', methods=['GET', 'POST'])
 def basket():
-    pass
+    try:
+        new_added_to_cart = {
+            'price': request.args.get('id'),
+            'quantity': request.form.get('quantity'),
+        }
+
+        cart = session['basket']
+
+        session['basket'] = [new_added_to_cart, cart]
+        print(session['basket'])
+
+    except KeyError:
+        print(2)
+        session['basket'] = {
+            'price': request.args.get('id'),
+            'quantity': request.form.get('quantity'),
+        }
+    return redirect(url_for("shop.shop_page"))
 
 
-# stripe.Product.create(
-#   name="Strawberry",
-#   default_price_data={
-#     "unit_amount": 10000,
-#     "currency": "try",
-#   },
-#   expand=["default_price"],
-# )
+def create_stop_item(productName, price, currency='usd'):
+    print(stripe.Product.create(
+        name=productName,
+        default_price_data={
+            "unit_amount": price,
+            "currency": currency,
+        },
+        expand=["default_price"],
+    ))
